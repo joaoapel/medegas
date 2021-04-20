207943be166e102bd809e5d9910d2475e30bb91f
João Fernando Apel Miguel <joaoapel@gmail.com>
2021-04-19 22:46:56 -0300

    Criação dos arquivos iniciais. Na pasta resources estão os arquivos de áudio, no formato .ogg.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to olhaogas
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/joaoapel/olhaogas_test.clj
@@ -0,0 +1,7 @@
+(ns joaoapel.olhaogas-test
+  (:require [clojure.test :refer :all]
+            [joaoapel.olhaogas :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
