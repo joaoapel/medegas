f6c75c5c4ac95b383cc35b02782c58cc045ddae8
João Fernando Apel Miguel <joaoapel@gmail.com>
2021-04-22 02:36:46 -0300

    O arquivo deps.edn foi modificado para importar a dependência local ({:local/root]). Foram importadas algumas classes, faltando ainda a classe PitchProcessor que possue um construtor (usar reify ou proxy).

--- src/joaoapel/olhaogas.clj
+++ src/joaoapel/olhaogas.clj
@@ -1,7 +1,50 @@
 (ns joaoapel.olhaogas
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.java.io :as io])
+  (:import (be.tarsos.dsp.pitch PitchDetectionResult PitchProcessor)
+           (be.tarsos.dsp.io.jvm AudioDispatcherFactory)))
+
+(defn processor [x]
+  PitchProcessor x)
+
+(defn detect [x]
+  PitchDetectionResult x)
+
+(defn import-audio-tarsos [x buffersize bufferoverlap]
+  (AudioDispatcherFactory/fromFile x y z))
+
+(comment
+  Parameters:
+    sampleRate - The requested sample rate must be supported by the capture device. Nonstandard sample rates can be problematic!
+    audioBufferSize - The size of the buffer defines how much samples are processed in one step. Common values are 1024,2048.
+    bufferOverlap - How much consecutive buffers overlap (in samples). Half of the AudioBufferSize is common.
+    Returns a new audioprocessor
+
+    ,)
+
+
+
+(defn import-audio-java [x]
+  (io/file x))
+
+
+(def file-cheio (import-audio-java "./cheio.wav"))
+(class file-cheio)
+;; => java.io.File
+
+(println file-cheio)
+
+(def file-tarsos (import-audio-tarsos file-cheio 2048 1024))
+
+(class file-tarsos)
+;; => be.tarsos.dsp.AudioDispatcher
+
+
+
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+;; => #'joaoapel.olhaogas/-main
+;; => #'joaoapel.olhaogas/-main
