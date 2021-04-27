9edf8e0770a6051e787dcb85994e401dfbc48f84
João Fernando Apel Miguel <joaoapel@gmail.com>
2021-04-26 22:05:23 -0300

    Foi feita a interop com o Java, conseguimos calcular a proporção de carga

--- src/joaoapel/olhaogas.clj
+++ src/joaoapel/olhaogas.clj
@@ -2,7 +2,7 @@
   (:gen-class)
   (:require [clojure.java.io :as io]
             [badigeon.javac :as javac])
-  (:import (be.tarsos.dsp.pitch PitchDetectionHandler PitchDetector PitchProcessor
+  (:import  (be.tarsos.dsp.pitch PitchDetectionHandler PitchDetector PitchProcessor
                                 PitchDetectionResult PitchProcessor$PitchEstimationAlgorithm)
            (be.tarsos.dsp.io.jvm AudioDispatcherFactory JVMAudioInputStream)
            (be.tarsos.dsp AudioDispatcher AudioEvent AudioProcessor)))
@@ -11,21 +11,51 @@
 ;;https://github.com/clojure/tools.deps.alpha/wiki/Tools
 ;;Clojure Extended: Java interop (English Edition), Ivan Grishaev
 
+
+(defn m [pitches]
+  (/ (apply + pitches) (count pitches)))
+
+
+
+
 ;;Usar reify na interface
 (defn detection-pitch [source]
   (let [dispatcher (AudioDispatcherFactory/fromFile (io/file source) 2048 1024)
         algorithm (PitchProcessor$PitchEstimationAlgorithm/YIN)
-        pitch-detection (PitchDetectionResult.)
+        pitch (atom[])
         pdh (reify PitchDetectionHandler
               (handlePitch [this result event]
-                (println result event)))
-        pitch (atom[])]
-    (-> dispatcher)))
+                (let [p (.getPitch result)]
+                  (when (<= 2600 p 3300)
+                    (swap! pitch conj p)))))
+
+        pitch-processor (PitchProcessor. algorithm 44100 2048 pdh)]
+    (.addAudioProcessor dispatcher pitch-processor)
+    (.run dispatcher)
+    (m @pitch)))
+
+(require '[clojure.java.shell :as sh])
+
+(sh/sh "pwd")
+
+(defn fullness [vazio cheio medido]
+  (let [d (- vazio cheio)
+        m (- vazio medido)]
+    (/ m d)))
+
+(fullness (detection-pitch "./vazio.wav") (detection-pitch "./cheio.wav") 3054)
+
+(detection-pitch "./vazio.wav")
+;; => 3222.609619140625
+
+
+(detection-pitch "./usado.wav")
+;; => 3054.1989822387695
 
 
 
 (detection-pitch "./cheio.wav")
-
+;; => 2755.599168346774
 
 (comment
   "
@@ -40,25 +70,26 @@
     adp.addAudioProcessor(new PitchProcessor(PitchEstimationAlgorithm.YIN, 44100, 2048, handler));
     adp.run();
 ")
+;; => nil
 
 
 
 ;;-----------------------------------------------------------------
-;;TESTE COM A BIBLIOTECA BADIGEON
+;;TESTE COM A BIBLIOTECA BADIGEON (USAR CÓDIGO JAVA DIRETO)
 ;;-----------------------------------------------------------------
 ;; Compile java sources under the src-java directory
 
+(comment
+  (defn java-compiler [x]
+    (javac/javac x {;; Emit class files to the target/classes directory
+                    :compile-path "target/classes"
+                    ;; Additional options used by the javac command
+                    :javac-options ["-cp" "src:target/classes" "-target" "7"
+                                    "-source" "11" "-Xlint:-options"]}))
 
-(defn java-compiler [x]
-  (javac/javac x {;; Emit class files to the target/classes directory
-                  :compile-path "target/classes"
-                  ;; Additional options used by the javac command
-                  :javac-options ["-cp" "src:target/classes" "-target" "7"
-                                  "-source" "11" "-Xlint:-options"]}))
 
 
-
-(java-compiler "src-java/TarsosDSP/")
+  (java-compiler "src-java/TarsosDSP/"))
 ;;-----------------------------------------------------------------
 ;;-----------------------------------------------------------------
 
